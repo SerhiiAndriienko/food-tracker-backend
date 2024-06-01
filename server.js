d63775bf049e0c85204f884235a905427ad15600
Serhii Andriienko <116962959+SerhiiAndriienko@users.noreply.github.com>
2024-06-01 18:21:39 +0200

    update port

--- server.js
+++ server.js
@@ -7,10 +7,8 @@ mongoose.set('strictQuery', true)
 mongoose
 	.connect(DB_HOST, { useNewUrlParser: true, useUnifiedTopology: true })
 	.then(() => {
-		app.listen(PORT),
-			() => {
-				console.log(`Server running. Use our API on port: ${PORT}`)
-			}
+		app.listen(PORT)
+		console.log(`Server running. Use our API on port: ${DB_HOST} ${PORT} `)
 	})
 	.catch(err => {
 		console.error('Database connection error:', err.message)
