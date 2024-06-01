e0aebab60722af4ac6e4e263c5143959e54822c0
Serhii Andriienko <116962959+SerhiiAndriienko@users.noreply.github.com>
2024-06-01 18:09:01 +0200

    update port

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose')
 const app = require('./app')
-const { DB_HOST, HOST } = process.env
+const { DB_HOST, PORT } = process.env
 
 mongoose.set('strictQuery', true)
 
 mongoose
 	.connect(DB_HOST)
 	.then(() => {
-		app.listen(HOST)
-		console.log(`Server  running. Use our API on port: ${HOST}`)
+		app.listen(PORT)
+		console.log(`Server  running. Use our API on port: ${PORT}`)
 	})
 	.catch(err => {
 		console.error(err.message)
